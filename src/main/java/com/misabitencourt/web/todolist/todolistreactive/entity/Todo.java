ee511d8a2bd91f826829d75e62bc92fdb2cc67e7
Misael Braga de Bitencourt <misael.bitencourt@videosoft.com.br>
2022-08-01 14:08:11 -0300

    Adding Lombok

--- src/main/java/com/misabitencourt/web/todolist/todolistreactive/entity/Todo.java
+++ src/main/java/com/misabitencourt/web/todolist/todolistreactive/entity/Todo.java
@@ -3,60 +3,29 @@ package com.misabitencourt.web.todolist.todolistreactive.entity;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 
-
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Todo extends AbstractPersistable<Long> {
 
     private static final long serialVersionUID = -2952735933715107252L;
-
     private String text;
 	private Date createdAt;
     private Date doneAt;
     private Date deletedAt;
-
-    public Todo() {}
 
     public Todo(String text, Date createdAt, Date doneAt, Date deletedAt) {
         this.text = text;
         this.createdAt = createdAt;
         this.doneAt = doneAt;
         this.deletedAt = deletedAt;
-    }
-
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getDoneAt() {
-        return doneAt;
-    }
-
-    public void setDoneAt(Date doneAt) {
-        this.doneAt = doneAt;
-    }
-
-    public Date getDeletedAt() {
-        return deletedAt;
-    }
-
-    public void setDeletedAt(Date deletedAt) {
-        this.deletedAt = deletedAt;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
 }
